a6ccc1695d8062b7872661cbf196f0e66cd1257d
arpita <arpitadhukia68@gmail.com>
2026-07-26 21:43:24 +0530

    add $ sub

---
+++ python/sum.py
@@ -0,0 +1,14 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+total=num1+num2
+print(f"the sum is{total}")
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+total=num1-num2
+print(f"the sum is{total}")
+
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+total=num1*num2
+print(f"the sum is{total}")
