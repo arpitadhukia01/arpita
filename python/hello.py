02c3dd137778c9895b73e57bd42cb0646f8950f9
arpita <arpitadhukia68@gmail.com>
2026-07-26 19:46:07 +0530

    first commit

---
+++ python/hello.py
@@ -0,0 +1,13 @@
+print("hello world")
+print("welcome to the program")
+
+x=10
+print(type(x))
+
+x="hello"
+print(type(x))
+
+students={1:"ram",2:"sham",3:"mohan"}
+print(type(students))
+print(students)
+
