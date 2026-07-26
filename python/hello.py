6feec28ac7b8ed7fbd59436bfbb8a572af620b79
arpita <arpitadhukia68@gmail.com>
2026-07-26 21:07:41 +0530

    day 1

--- python/hello.py
+++ python/hello.py
@@ -11,3 +11,14 @@ students={1:"ram",2:"sham",3:"mohan"}
 print(type(students))
 print(students)
 
+name=input("what is your name?")
+print("nice to meet you,"+name+"!")
+
+name="arpita"
+print(name)
+
+age=21
+print(age)
+
+fruits=["apple","banana","kiwi"]
+print(fruits)
